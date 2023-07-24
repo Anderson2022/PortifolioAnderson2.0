7e3a449733110c5c3b5d65bca50d73f346ffbca9
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2023-07-24 13:13:33 -0300

    Atualizar o Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -54,7 +54,7 @@ const Main = () => {
                 <AiOutlineMail />
               </div>
             </Link>
-            <Link href='/resume'>
+            <Link href='/'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:bg-orange-500 hover:scale-110 ease-in duration-300'>
                 <BsFillPersonLinesFill />
               </div>
