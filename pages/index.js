c0ee8d9bc2450963e987328dae634263fc8cf809
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2023-11-09 22:00:13 -0400

    feat: create resums

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Anderson | Full Stack Developer</title>
+        <title>Anderson | Full Stack Developer..</title>
         <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
         <link rel="icon" href="/fav.png" />
       </Head>
