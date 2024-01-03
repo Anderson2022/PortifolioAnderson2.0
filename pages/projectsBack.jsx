975b181e55fb30f7f099c1751559764b55d97791
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2024-01-03 13:11:19 -0400

    feat: organizing the system

--- pages/projectsBack.jsx
+++ pages/projectsBack.jsx
@@ -1,16 +1,78 @@
-import Image from 'next/image';
-import React from 'react';
-import Arte from '../public/assets/projects/industria.png'
-import rpg from '../public/assets/projects/jogo-rpg.png'
-import graduado from '../public/assets/projects/graduado.png'
-import sql from '../public/assets/projects/analise-de-consulta.png'
-import carro from '../public/assets/projects/carro-suv.png'
-import java from '../public/assets/projects/icons8-java-50.png'
-import Types from '../public/assets/projects/icons8-javascript-50.png'
-import Script from '../public/assets/projects/icons8-typescript-50.png'
-import pdf from '../public/assets/projects/pdf.png'
-import financeiro from '../public/assets/projects/fluxo-de-caixa.png'
+import { Image, React, Arte, japa, Types2, adonis, redis, rpg, graduado, sql, carro, java, Script, Types, pdf, financeiro, script, sqldata, cache, trader, next, nest, docker, kafka } from '../pages/scripts/imports';
 
+
+const projetos = [
+  {
+    id: 1,
+    nome: 'Roleplay-api',
+    url: ['https://www.typescriptlang.org/','https://adonisjs.com/', 'https://japa.dev/docs/introduction'],
+    imagem: rpg,
+    githubLink: 'https://github.com/Anderson2022/roleplay-api',
+    imagensArte: [Types2, adonis, japa],
+    descricao: 'Jogo de RPG. Implementando uma API para gerenciar mesas de RPG.',
+  },
+  {
+    id: 2,
+    nome: 'Universidade',
+    imagem: graduado,
+    url: ['https://www.typescriptlang.org/', 'https://adonisjs.com/', 'https://japa.dev/docs/introduction', 'https://redis.com/pt/solucoes/casos-de-uso/servico-de-mensagens/'],
+    githubLink: 'https://github.com/Anderson2022/Univercidade',
+    imagensArte: [Types2, adonis, japa, redis],
+    descricao: 'Implementando uma API para gerenciar cadastros de novos alunos ',
+  },
+  {
+    id: 3,
+    nome: 'Aluguel',
+    imagem: carro,
+    url: ['https://www.typescriptlang.org/',],
+    githubLink: 'https://github.com/Anderson2022/Alugueldecarros2.0',
+    imagensArte: [Types2],
+    descricao: 'Implementando uma API para registro de carros alugueis',
+  },
+  {
+    id: 4,
+    nome: 'Script',
+    url: 'https://github.com/Anderson2022/bat.sqlsever',
+    imagem: sql,
+    githubLink: 'https://github.com/Anderson2022/bat.sqlsever',
+    imagensArte: [script, sqldata,],
+    descricao: 'Script de atualização de banco de dados',
+  },
+  {
+    id: 5,
+    nome: 'Pdf',
+    imagem: pdf,
+    githubLink: 'https://github.com/Anderson2022/Pdf',
+    imagensArte: [pdf, Types2,],
+    descricao: 'Código de implementação de pdf',
+  },
+  {
+    id: 6,
+    nome: 'Financeiro-api',
+    imagem: financeiro,
+    githubLink: 'https://github.com/Anderson2022/ProjetoTypesript',
+    imagensArte: [Types2],
+    descricao: 'Sistema financeiro de estudo em Next.js',
+  },
+  {
+    id: 7,
+    nome: 'Estudos de Cache Backend',
+    imagem: cache,
+    githubLink: 'https://github.com/Anderson2022/cache_studies',
+    imagensArte: [Script],
+    descricao: 'Api de estudos de cache no backend',
+  },
+
+  {
+    id: 8,
+    nome: 'Home Broker',
+    imagem: trader,
+    githubLink: 'https://github.com/Anderson2022/Homebroker',
+    imagensArte: [next, nest, docker, kafka],
+    descricao: 'Estudos de mensageria com Go, Nest, Next, Kafka, Docker ',
+  },
+  // Adicione mais projetos conforme necessário
+];
 
 
 const Projects = () => {
@@ -48,248 +110,41 @@ const Projects = () => {
 
         <div className='w-screen flex justify-center items-center  '>
           <div className='grid grid-cols-4 gap-4'>
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={rpg} alt="" />
+            {projetos.map((projeto) => (
+              <div key={projeto.id} className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
+                <div className='flex flex-wrap p-2'>
+                  <div className='w-1/2'>
+                    <div className='w-20 rounded-full flex items-center justify-center'>
+                      <Image className="rounded-lg mx-auto drop-shadow-xl" src={projeto.imagem} alt="" />
+                    </div>
+                  </div>
+                  <div className='w-1/2'>
+                    {projeto.nome}
                   </div>
                 </div>
-                <div className='w-1/2'>
-                  Roleplay-api
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="https://github.com/Anderson2022/roleplay-api">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
+                <div className='flex flex-wrap p-2'>
+                  <div className='w-1/2'>
+                    <a href={projeto.githubLink}>Verificar</a>
+                  </div>
+                  <div className='w-1/2'>
+                    <div className='flex'>
+                      {projeto.imagensArte.map((arte, index) => (
+                        <a key={index} href={projeto.url && projeto.url[index]} target="_blank" rel="noopener noreferrer">
+                          <div className='w-8 px-1 relative'>
+                            <Image className="mx-auto drop-shadow-xl w-full h-full object-cover" src={arte} alt="" />
+                          </div>
+                        </a>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className='p-2'>
-                <h5>Jogo de rpg</h5>
-                <p className='text-xs'>Implementando uma API para gerenciar mesas de RPG.</p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={graduado} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                  Universidade
+                <div className='p-2'>
+                  <h5>{projeto.nome}</h5>
+                  <p className='text-xs'>{projeto.descricao}</p>
                 </div>
               </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="https://github.com/Anderson2022/Univercidade">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Sistema de cadastro</h5>
-                <p className='text-xs'>Implementando uma API para gerenciar cadastros de novos alunos </p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={carro} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                  Aluguel
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="https://github.com/Anderson2022/Alugueldecarros2.0">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Sistema de aluguel de carros</h5>
-                <p className='text-xs'>Implementando uma API para registro de carros alugueis </p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={sql} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                 Script
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="https://github.com/Anderson2022/bat.sqlsever">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Atualização</h5>
-                <p className='text-xs'>Script de atualização de banco de dados</p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={pdf} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                  Pdf
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="https://github.com/Anderson2022/Pdf">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Código de implementação de pdf</h5>
-                <p className='text-xs'>Implementando uma API para gerenciar mesas de RPG.</p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={financeiro} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                  Financeiro-api
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="https://github.com/Anderson2022/ProjetoTypesript">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Estudo</h5>
-                <p className='text-xs'>Sistema financeiro de estudo em Next.js</p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                  Roleplay-api
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="http://">Verificar</a>
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Jogo de rpg</h5>
-                <p className='text-xs'>Implementando uma API para gerenciar mesas de RPG.</p>
-              </div>
-            </div>
-
-            <div className='shadow-lg h-56 w-72 border-8 border-opacity-25 transform transition-transform duration-300 hover:scale-105'>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <div className='w-20 rounded-full flex items-center justify-center'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-                <div className='w-1/2'>
-                  Roleplay-api
-                </div>
-              </div>
-              <div className='flex flex-wrap p-2'>
-                <div className='w-1/2'>
-                  <a href="http://">Verificar</a> 
-                </div>
-                <div className='w-1/2'>
-                  <div className=' flex'>
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                    <Image className="rounded-lg mx-auto drop-shadow-xl" src={Arte} alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className='p-2'>
-                <h5>Jogo de rpg</h5>
-                <p className='text-xs'>Implementando uma API para gerenciar mesas de RPG.</p>
-              </div>
-            </div>
-
+            ))}
           </div>
-
         </div>
       </div>
 
