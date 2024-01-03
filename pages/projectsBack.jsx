e708a2b5c533ba8bd7524bc8ebae2750d5a0dfe5
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2024-01-03 13:24:03 -0400

    feat: organizing the system

--- pages/projectsBack.jsx
+++ pages/projectsBack.jsx
@@ -1,5 +1,27 @@
-import { Image, React, Arte, japa, Types2, adonis, redis, rpg, graduado, sql, carro, java, Script, Types, pdf, financeiro, script, sqldata, cache, trader, next, nest, docker, kafka } from '../pages/scripts/imports.js';
-
+import Image from 'next/image';
+import React from 'react';
+import Arte from '../public/assets/projects/industria.png'
+import japa from '../public/assets/projects/japa.png'
+import Types2 from '../public/assets/projects/typescript_5968381.png'
+import adonis from '../public/assets/projects/adonisjs.svg'
+import redis from '../public/assets/projects/redis.svg'
+import rpg from '../public/assets/projects/jogo-rpg.png'
+import graduado from '../public/assets/projects/graduado.png'
+import sql from '../public/assets/projects/analise-de-consulta.png'
+import carro from '../public/assets/projects/carro-suv.png'
+import java from '../public/assets/projects/icons8-java-50.png'
+import Script from '../public/assets/projects/icons8-javascript-50.png'
+import Types from '../public/assets/projects/icons8-typescript-50.png'
+import pdf from '../public/assets/projects/pdf.png'
+import financeiro from '../public/assets/projects/fluxo-de-caixa.png'
+import script from '../public/assets/projects/script.svg'
+import sqldata from '../public/assets/projects/database-sql.svg'
+import cache from '../public/assets/projects/cache.png'
+import trader from '../public/assets/projects/trader.png'
+import next from '../public/assets/projects/nextjs.svg'
+import nest from '../public/assets/projects/nestjs.svg'
+import docker from '../public/assets/projects/docker.svg'
+import kafka from '../public/assets/projects/kafka.svg'
 
 const projetos = [
   {
