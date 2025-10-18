e41ce5c9dd392902ff95fa03f6221832bdffa722
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2025-10-18 15:14:48 -0400

    oi

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -83,10 +83,7 @@ const Menu = ({ linkColor }) => {
             </li>
             <li className='button-89 hover:border-b text-sm font-bold tracking-wide uppercase font-mono transition-colors duration-200 hover:text-blue-600' onClick={() => setIsOpen(false)}>
               <Link href='/#contact'>Contact</Link>
-            </li>
-            <li className='button-89 hover:border-b text-sm font-bold tracking-wide uppercase font-mono transition-colors duration-200 hover:text-blue-600' onClick={() => setIsOpen(false)}>
-              <Link href='/test'>Visitantess</Link>
-            </li>
+            </li>    
           </ul>
         </div>
       )}
